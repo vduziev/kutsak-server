006d3f517748bbecf769cb6645da27391554df31
Marsoau <koteika5063@gmail.com>
2026-07-13 11:18:47 +0300

    Update webflow form validation

--- Kutsak.Server/Controllers/WebflowController.cs
+++ Kutsak.Server/Controllers/WebflowController.cs
@@ -24,20 +24,18 @@ public class WebflowController : KutsakControllerBase
 
     private void VerifyWebflow(string body) {
         if (!Request.Headers.TryGetValue("x-webflow-timestamp", out var timestamp) ||
-            !Request.Headers.TryGetValue("x-webflow-signature", out var providedSignature))
-        {
+            !Request.Headers.TryGetValue("x-webflow-signature", out var providedSignature)
+        ) {
             throw new UnauthorizedAccessException();
         }
         
         string calculatedSignature;
-        using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(_wfToken)))
-        {
+        using (var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(_wfToken))) {
             var hashBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes($"{timestamp}:{body}"));
             calculatedSignature = Convert.ToHexString(hashBytes).ToLower();
         }
         
-        if (!calculatedSignature.Equals(providedSignature.ToString(), StringComparison.CurrentCultureIgnoreCase))
-        {
+        if (!calculatedSignature.Equals(providedSignature.ToString(), StringComparison.CurrentCultureIgnoreCase)) {
             throw new UnauthorizedAccessException();
         }
     }
@@ -55,9 +53,9 @@ public class WebflowController : KutsakControllerBase
         if (submission is null) return BadRequest("Cannot serialize to a webflow submission");
         if (submission.TriggerType != "form_submission") return BadRequest("Not a form submission");
         
-        var form = JsonSerializer.Deserialize<FormSubmission>(submission.Payload)!;
+        var form = submission.Payload.Deserialize<FormSubmission>()!;
         
-        if (form.Name != "Contact Us") return BadRequest("Not a contact us form");
+        if (form.Name != "Contact Us Email Form") return BadRequest("Not a contact us form");
         
         Console.WriteLine($"Submission: {submission.TriggerType}");
         Console.WriteLine($"Form: {form.FormId}");
@@ -69,8 +67,6 @@ public class WebflowController : KutsakControllerBase
             <b>Ім'я:</b> {form.Data.CustomerName}
             <b>Е. Пошта:</b> {form.Data.CustomerEmail}
             <b>Телефон:</b> <code>{form.Data.PhoneNumber}</code>
-            
-            <b>Вид Послуги:</b> <i>{form.Data.Service}</i>
             
             <b>Повідомлення:</b> {(string.IsNullOrWhiteSpace(form.Data.CustomerMessage) ? "<i>no message</i>" : "")}
             {form.Data.CustomerMessage}
